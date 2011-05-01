f90df69ee2e2a10110a857320b19489c8f04983a
Paul Asmuth <paul@23linesofcode.com>
2011-05-01 16:29:01 +0200

    adding stubs

--- lib/mtgox-ruby.rb
+++ lib/mtgox-ruby.rb
@@ -1,4 +1,48 @@
 class MtgoxClient
 
+  def initialize
+  end
+	
+  def ticket_data
+    raise "implement me"
+  end
+	
+  def market_depth
+    raise "implement me"
+  end
+	
+  def recent_trades
+    raise "implement me"
+  end
+	
+  def current_balance
+    raise "implement me"
+  end
+  
+  def open_orders
+    raise "implement me"
+  end
+  
+  def cancel_order(order_id, buy_or_sell)
+    raise "implement me"
+  end
+  
+  def place_order(amount, price, buy_or_sell)
+    raise "implement me"
+  end  
+  
+  def send_bitcoin(amount, bitcoin_address)
+    raise "implement me"
+  end
+  
+private
+  
+  def place_buy_order(amount, price)
+    raise "implement me"
+  end
+  
+  def place_sell_order(amount, price)
+    raise "implement me"
+  end
 
 end
